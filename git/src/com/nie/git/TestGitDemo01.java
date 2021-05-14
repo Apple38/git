789b515f689f216dfc427d758c493e068dd2ffea
Apple38 <941189484@qq.com>
2021-05-14 17:14:58 +0800

    将项目提交到版本库 我修改了TestGitDemo01，并将项目推送到远程仓库。

--- git/src/com/nie/git/TestGitDemo01.java
+++ git/src/com/nie/git/TestGitDemo01.java
@@ -3,5 +3,6 @@ package com.nie.git;
 public class TestGitDemo01 {
     public static void main(String[] args) {
         System.out.println("这里有一个可爱的小花猫。");
+        System.out.println("我修改了TestGitDemo01");
     }
 }
